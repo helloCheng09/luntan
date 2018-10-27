450cb1fa9f3a08fc5c9d823fe1624f3a52193a16
po_chen <1814638551@qq.com>
2018-10-27 17:02:14 +0800

    update 20181027

--- src/js/index.js
+++ src/js/index.js
@@ -2,29 +2,138 @@ let $ = window.Zepto
 let $scope = $(window.document)
 let root = window.topic
 
-$scope.on("click", "#fabuSubmit", function () {
-    layer.alert('发布失败，请完善接龙信息！', {
-        skin: 'layui-layer-blue',
-        closeBtn: 0
-    });
-})
-// 文本框自适应
-if (document.getElementsByTagName("textarea").length) {
+
+/***************************************************************************** */
+/**
+ * 提交页面 入口
+ */
+
+if ($("#fqWrap").length) {
+
+    $scope.on("click", "#fabuSubmit", function () {
+        layer.alert('发布失败，请完善接龙信息！', {
+            skin: 'layui-layer-blue',
+            closeBtn: 0
+        });
+    })
+
+    // 文本框自适应
     let textaraeEle = document.getElementsByTagName("textarea")
     root.autoTextarea(textaraeEle[0])
-}
 
-if ($(".teacher-item").length) {
     // 选择班级、学生
     root.multiSelect($(".teacher-item"), "selected")
     root.selectAllBtn($("#selAll"), "btn_selected")
     root.singleSel($(".student-item"), "selected")
 }
 
+/***************************************************************************** */
+/**
+ * 接龙话题列表 入口
+ */
 if ($("#tListWra").length) {
-    let src = "../img/jielonglogo.png"
-    // let src="/public/yz/jl_img/jielonglogo.png"
+    // let src = "../img/jielonglogo.png"
+    let src="/public/yz/jl_img/jielonglogo.png"
     root.renderImg(src)
 
     console.log(root)
+}
+
+/***************************************************************************** */
+/**
+ * 接龙话题详情 入口
+ */
+
+if ($("#tDetWrap").length) {
+    //  评论和点赞 列表展示切换
+    $(".top-tags .com-tag").on("click", function () {
+        let flag = $(this).hasClass("checked")
+        if (!flag) {
+            $(".like-tag").removeClass("checked")
+            $(".like-list").css("display", "none")
+            $(this).addClass("checked")
+            $(".com-list").css("display", "block")
+            console.log("评论列表")
+        }
+    })
+    $(".top-tags .like-tag").on("click", function () {
+        let flag = $(this).hasClass("checked")
+        if (!flag) {
+            $(".com-tag").removeClass("checked")
+            $(".like-list").css("display", "block")
+            $(this).addClass("checked")
+            $(".com-list").css("display", "none")
+            console.log("点赞列表")
+        }
+    })
+
+    // 判断是否点赞
+    $(".com-like").each(function () {
+        let islike = $(this).attr("like-status")
+        console.log(islike)
+        if (islike != 0) {
+            $(this).removeClass("nozan")
+            $(this).addClass("hadzan")
+            // $(this).css("backgroundPosition","100% 100%")
+        } else {
+            $(this).addClass("nozan")
+            $(this).removeClass("hadzan")
+            // 点赞效果
+            root.clickLike()
+            // $(this).css("backgroundPosition","0% 100%")
+        }
+    })
+    // 回复接龙主题
+    $("#comThis").on("click", function () {
+        $("#subForm_1").css("display", "flex")
+        // let huiObj = "写回复 " +  $("#tDetWrap").find(".xz-text").text()
+        let huiObj = "写回复 "
+        $("#subForm_1").find("textarea").attr("placeholder", huiObj)
+    })
+    $("#subForm_1 .blank-close").on("click", function () {
+        $("#subForm_1").css("display", "none")
+    })
+
+    // 回复评论
+    $(".com-item").each(function () {
+        let ele = $(this)
+        $(this).find(".com-text").on("click", function () {
+            console.log("回复评论")
+            let huiObj = "回复 " + ele.find(".nick").text() + " 的接龙内容"
+            $("#subForm_2").css("display", "flex")
+            console.log(huiObj)
+            $("#subForm_2").find("textarea").attr("placeholder", huiObj)
+        })
+    })
+    $("#subForm_2 .blank-close").on("click", function () {
+        $("#subForm_2").css("display", "none")
+    })
+
+
+    // 文本框自适应
+    let textaraeEle = document.getElementsByTagName("textarea")
+    root.autoTextarea(textaraeEle[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/js/render.js
+++ src/js/render.js
@@ -9,6 +9,25 @@
         }
     }
 
+    function clickLike() {
+        $(".com-like").on("click", function () {
+            let islike = $(this).attr("like-status")
+            if (islike == 0) {
+                let num = Number($(this).next(".like-num").text()) + 1
+                console.log(num)
+                $(this).attr("like-status", "1")
+                $(this).addClass("addlike")
+                $(this).next(".like-num").text(num)
+            }
+
+        })
+    }
+
+
+    // 模糊背景
     root.renderImg = renderImg
-    
+
+    // 点赞
+    root.clickLike = clickLike
+
 }(window.Zepto, window.topic || (window.topic = {})))
