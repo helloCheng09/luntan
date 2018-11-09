6d6b9c552722e468d31c63bc905877ac07a74b0a
po_chen <1814638551@qq.com>
2018-11-09 14:13:28 +0800

    update 20181109

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ let uglify = require("gulp-uglify");
 let cleanCSS = require('gulp-clean-css')
 let jsRemove = require('gulp-remove-logging')
 var strip = require("gulp-strip-debug")
+let concat = require('gulp-concat');
 
 const folder = {
     src: "./src/",
@@ -29,21 +30,27 @@ gulp.task("img", () => {
     return gulp.src(folder.src + "img/*")
         .pipe(gulp.dest(folder.build + "img"))
 })
+// concat
+gulp.task("concat", () => {
+    return gulp.src([folder.src + "js/wx.jsdk.js",folder.src + "js/click.js", folder.src + "js/render.js", folder.src + "js/getData.js", folder.src + "js/index.js"])
+        .pipe(concat('jl_all.js'))
+        .pipe(gulp.dest(folder.src + "js"))
+})
 // js
 gulp.task("js", () => {
     return gulp.src(folder.src + "js/*.js")
-        // .pipe(jsRemove())
         .pipe(strip())
         .pipe(babel({
             presets: ['es2015']
         }))
-        // .pipe(uglify())
+        .pipe(uglify())
         .pipe(gulp.dest(folder.build + "js"))
 })
+
 // layui
 gulp.task("layui", () => {
     return gulp.src(folder.src + "js/layui/")
         .pipe(gulp.dest(folder.build + "js/layui/"))
 })
 // gulp.task("default", ["html", "css", "img", "js", "layui"])
-gulp.task("default", ["css", "js"])
+gulp.task("default", ["css","concat"])
